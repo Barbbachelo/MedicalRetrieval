2abd9dd35d370f9af54d7119ba0f2421169d6fed
Barbbachelo <nckhansn@gmail.com>
2015-04-30 17:38:24 +1000

    Added rest of files

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function addMouseOver() {
+    document.getElementById("add").src="images/addclicked.png"
+}
+
+function addMouseOut() {
+    document.getElementById("add").src="images/addpatient.png"
+}
