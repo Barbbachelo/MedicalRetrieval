d33cc301e690cab3e3c7c67f42bf475928b06361
Barbbachelo <nckhansn@gmail.com>
2015-04-30 13:53:53 +1000

    Revert "Prototype"
    This reverts commit c8f0bd78c845aa9cc7346725197f8fddd9077c22.

--- script.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-function addMouseOver() {
-    document.getElementById("add").src="images/addclicked.png"
-}
-
-function addMouseOut() {
-    document.getElementById("add").src="images/addpatient.png"
-}
